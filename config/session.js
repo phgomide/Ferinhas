933df1586a1fbcac94f570d331f700fd63f4fb18
PertinaciousMedici <pertinaciouslymedici@gmail.com>
2026-05-05 19:31:47 +0000

    issues with render

--- config/session.js
+++ config/session.js
@@ -1,15 +1,21 @@
 const session = require('express-session');
 const crypto = require('crypto');
+const SQLiteStore = require('connect-sqlite3')(session);
 
 module.exports = session({
-    secret: process.env.SESSION_SECRET || crypto.randomBytes(32).toString('hex'),
-    name: 'sessionId',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        httpOnly: true,
-        sameSite: 'strict',
-        secure: process.env.NODE_ENV === 'production',
-        maxAge: 1000 * 60 * 60 * 24
-    }
+  store: new SQLiteStore({
+    db: 'sessions.sqlite',
+    dir: './',
+    table: 'sessions'
+  }),
+  secret: process.env.SESSION_SECRET || crypto.randomBytes(32).toString('hex'),
+  name: 'sessionId',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 1000 * 60 * 60 * 24 // 1 day
+  }
 });
